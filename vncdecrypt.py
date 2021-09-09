9f52c02fc8abbdcdab89731eaa0df983e912c335
Nikola Pepelishev <nikola.pepelishev@azdio.com>
2021-09-09 22:42:56 +0300

    flag checks

--- vncdecrypt.py
+++ vncdecrypt.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 import sys,pyDes
 
+def usage():
+	print("[-] Usage: ",str(sys.argv[0])," -i file / -s hex string")
+
 key = [232, 74, 214, 96, 196, 114, 26, 224]
 print("VNC Password Decryptor")
 print("----------------------")
 if(len(sys.argv)<3):
-	print("[-] Usage: ",str(sys.argv[0])," -i file / -s hex string")
+	usage()
 	sys.exit(1)
 
 mode = str(sys.argv[1])
@@ -15,8 +18,13 @@ if(mode.lower() == "-i"):
 	print("[*] Opening file: ",ifile)
 	with open(ifile,'rb') as fd:
 		pwd=fd.read()
+
+elif(mode.lower() == "-s"):
+	pwd = bytearray.fromhex(sys.argv[2])
+
 else:
-	pwd = bytearray. fromhex(sys.argv[2])
+	usage()
+	sys.exit(1)
 
 print("[*] Data: ",pwd)
 des = pyDes.des(key)
