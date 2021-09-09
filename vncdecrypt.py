155be9213b725b5214af0a98faf89fb96250bb05
Nikola Pepelishev <nikola.pepelishev@azdio.com>
2021-09-09 22:35:18 +0300

    Added hex string decrypt from cmdline

--- vncdecrypt.py
+++ vncdecrypt.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
 import sys,pyDes
 
-print("[*] VNC Password File Decryptor [*]\n")
-if(len(sys.argv)<2) :
-	print("[-] File name required.")
+key = [232, 74, 214, 96, 196, 114, 26, 224]
+print("VNC Password Decryptor")
+print("----------------------")
+if(len(sys.argv)<3):
+	print("[-] Usage: ",str(sys.argv[0])," -i file / -s hex string")
 	sys.exit(1)
 
-ifile = str(sys.argv[1])
-key = [232, 74, 214, 96, 196, 114, 26, 224]
+mode = str(sys.argv[1])
 
-print("[*] Opening file: ",ifile)
-with open(ifile,'rb') as fd:
-	pwd=fd.read()
-	print("[*] Data read: ",pwd)
-	des = pyDes.des(key)
-	dpwd = des.decrypt(pwd)
+if(mode.lower() == "-i"):
+	ifile = str(sys.argv[2])
+	print("[*] Opening file: ",ifile)
+	with open(ifile,'rb') as fd:
+		pwd=fd.read()
+else:
+	pwd = bytearray. fromhex(sys.argv[2])
 
-	print("[*] Decrypted pwd: ",dpwd.decode("utf-8"))
+print("[*] Data: ",pwd)
+des = pyDes.des(key)
+dpwd = des.decrypt(pwd)
+
+print("[*] Decrypted pwd: ",dpwd.decode("utf-8"))
